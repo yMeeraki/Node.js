9b2c526c9dc6971ab2743c3623bb21153435052e
Yukti Gupta <yuktigupta95@gmail.com>
2026-01-13 17:37:58 +0530

    Executing first .js file

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const tutorial = require('./tutorial')
-console.log(tutorial)
-console.log(tutorial(1,2))
+const fs = require('fs') 
+fs.writeFile('output.txt', "Writing File", (err) => {
+    if(err){
+        console.log("Error Occurred")
+    }
+    else{
+        console.log("File Written Successfully")
+    }
+})
