98c5243b678314cf1a9978c4fe9d10564d8e94c4
Yukti Gupta <yuktigupta95@gmail.com>
2026-01-21 13:05:02 +0530

    File System intro

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-const fs = require('fs') 
-fs.writeFile('output.txt', "Writing File", (err) => {
-    if(err){
-        console.log("Error Occurred")
-    }
-    else{
-        console.log("File Written Successfully")
-    }
-})
+const fs =require("fs")
+fs.writeFileSync("yuktiGupta.txt","Hello world, this is Yukti.")
