359699e4f4a30875dc4c308fc370d9a66a2cf8bf
Yukti Gupta <yuktigupta95@gmail.com>
2026-01-21 16:30:13 +0530

    nodemon

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Basic Server
 
 const http = require("http")
-const http2 = require("http")
+// const http2 = require("http")
 
 http.createServer((req,res) => {
-  res.write("<h1>hello world</h1>")
-  res.end("WOW!!")  
+  res.setHeader("Content-Type","text/html")
+  res.write("<h2>hello world</h2>")
+  res.end("WOW!! OMG OMG")  
 }).listen(2000)
 
-http2.createServer((req,res) => {
-  res.end("We!!!")
-}).listen(3000)
+// http2.createServer((req,res) => {
+//   res.end("We!!!")
+// }).listen(3000)
